8f2896f8652375565c54378ea55d7b1b8dff5981
Jason Shultz <jasshultz@gmail.com>
2013-09-19 11:28:08 -0600

    pdfs are downloaded to the server now

--- forms/application/config/autoload.php
+++ forms/application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session','user_agent');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('file', 'url');
 
 
 /*
